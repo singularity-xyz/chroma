617a9d70231fd2d722d5fb9bc50ad54cc751210c
Bebop <nneven@usc.edu>
2023-07-29 22:10:29 -0700

    Revert "Update app.py"
    This reverts commit dfbdf4a8dd0891844909d9db183d8e737be6c89f.

--- chromadb/app.py
+++ chromadb/app.py
@@ -3,7 +3,5 @@ import chromadb.config
 from chromadb.server.fastapi import FastAPI
 
 settings = chromadb.config.Settings()
-# enable SSL for Railway
-settings.chroma_server_ssl_enabled = True
 server = FastAPI(settings)
 app = server.app()
